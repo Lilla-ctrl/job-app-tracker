57824e6f632406b72728b721a4b8de6e74780e27
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-07-21 19:27:27 +0200

    Update Jobcard.jsx

--- src/components/Jobcard.jsx
+++ src/components/Jobcard.jsx
@@ -1,3 +1,16 @@
 export default function Jobcard() {
-  return <div>card</div>
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+      <div className="bg-gray-100 p-6 rounded-xl shadow-md border border-gray-200">
+        <h2>Company</h2>
+        <div className="grid grid-cols-2 ">
+          <h3>Position</h3>
+          <h3>Date</h3>
+        </div>
+        <h3>Status</h3>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </div>
+  );
 }
