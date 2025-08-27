1b1f08e6232a79d03089737b9d46a11859832c30
Lilla-ctrl <berenyi.lilu@gmail.com>
2025-08-27 17:58:16 +0200

    Buttons updated

--- src/components/Jobcard.jsx
+++ src/components/Jobcard.jsx
@@ -43,13 +43,13 @@ export default function Jobcard({ jobData, onEdit, onDelete, isLoaded }) {
           <div className="flex flex-wrap gap-3 justify-end mt-auto">
             <button
               onClick={() => onEdit(index)}
-              className="border-1 border-gray-300 rounded-md px-3 py-1"
+              className="border-1 border-gray-300 rounded-md px-3 py-1 cursor-pointer hover:bg-gray-200"
             >
               Edit
             </button>
             <button
               onClick={() => onDelete(index)}
-              className="border-1 border-gray-300 rounded-md px-3"
+              className="border-1 border-gray-300 rounded-md px-3 cursor-pointer hover:bg-gray-200"
             >
               Delete
             </button>
